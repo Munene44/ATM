401109816dd8b60e71505856494261d70ca45fd8
nenekelvin <karvinmitchar@gmail.com>
2019-11-14 19:20:14 +0300

    same notes

--- atm.rb
+++ atm.rb
@@ -7,3 +7,6 @@ end
 array = []
  notes.each_with_index do |note,index|
  	number_of_notes = amount / note
+ 	
+    next_note = notes[index+1]
+    number_of_notes -= 1 if next_note && ((amount % note)%next_note) != 0
