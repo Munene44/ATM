22bff41211ab6c67a7f6f3724771eabeb8d31d99
nenekelvin <karvinmitchar@gmail.com>
2019-11-15 07:06:36 +0300

    withdrawal rate

--- 10bill.rb
+++ 10bill.rb
@@ -1,2 +1,20 @@
 interest_rate = 4.45
 balance = balance * interest_rate
+
+describe 'atm' do
+  [
+    [-1, false],
+    [0, false],
+    [7, false],
+    [53, false],
+    [35, [20, 10, 5]],
+    [40, [20, 20]],
+    [65, [20, 20, 20, 5]],
+    [70, [20, 20, 20, 10]],
+    [75, [20, 20, 20, 10, 5]],
+  ].each do |input, expected|
+    it "should return #{expected} when $#{input} is withdrawn" do
+      withdraw(input).must_equal expected
+    end
+  end
+end
